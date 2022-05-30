dcadd32c80901116edeb5e0eae584495be357e79
Seanxyc <seanxyc41@gmail.com>
2022-05-30 23:56:46 +0800

    colorscheme

--- init.lua
+++ init.lua
@@ -1,3 +1,5 @@
 require "yaroga.options"
 require "yaroga.keymaps"
+require "yaroga.plugins"
+require "yaroga.colorscheme"
 
---
+++ lua/yaroga/colorscheme.lua
@@ -0,0 +1,15 @@
+if ((vim.api.nvim_eval('exists("&termguicolors")')) and (vim.api.nvim_eval('exists("&winblend")'))) then
+  vim.cmd [[
+    syntax enable
+    set termguicolors
+    set winblend=0
+    set wildoptions=pum
+    set pumblend=5
+    set background=dark
+    let g:neosolarized_termtrans=1
+    runtime ./colors/NeoSolarized.vim
+    colorscheme NeoSolarized
+  ]]
+end
+
+
