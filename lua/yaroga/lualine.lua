a38289cd4598ab033bdee45688b25ff7ab030740
Seanxyc <seanxyc41@gmail.com>
2022-06-03 18:33:03 +0800

    alpha

--- lua/yaroga/lualine.lua
+++ lua/yaroga/lualine.lua
@@ -65,7 +65,7 @@ lualine.setup({
 		theme = "solarized_dark",
 		component_separators = { left = "", right = "" },
 		section_separators = { left = "", right = "" },
-		disabled_filetypes = { "dashboard", "NvimTree", "Outline" },
+		disabled_filetypes = {"alpha", "dashboard", "NvimTree", "Outline" },
 		always_divide_middle = true,
 	},
 	sections = {
--- lua/yaroga/plugins.lua
+++ lua/yaroga/plugins.lua
@@ -55,6 +55,8 @@ return packer.startup(function(use)
   use "ahmedkhalf/project.nvim"
   use 'lewis6991/impatient.nvim'
   use "lukas-reineke/indent-blankline.nvim"
+  use {'goolord/alpha-nvim', requires = {'kyazdani42/nvim-web-devicons'}}
+  use "antoinemadec/FixCursorHold.nvim" -- This is needed to fix lsp doc highlight
 
 
   -- cmp plugins --
